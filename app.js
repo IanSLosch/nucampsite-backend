19606371a1a14e276e5d1b747b1d53200912126a
Ian S <ianslosch@gmail.com>
2023-06-12 15:10:36 -0400

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -11,6 +12,20 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017/nucampsite'
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false, 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+connect.then(() => console.log('Connected correctly to Server'),
+  err => console.log(err) // another way to handle errors (this could also be a .catch)
+)
 
 var app = express();
 
---
+++ models/campsite.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema // not required, but cleans the code up
+require('mongoose-currency').loadType(mongoose) // loads into mongoose, so it's available for schema's to use
+const Currency = mongoose.Types.Currency
+
+const commentSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  }, 
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }, 
+    image: {
+      type: String,
+      required: true
+    },
+    elevation: {
+      type: Number,
+      required: true
+    },
+    cost: {
+      type: Currency,
+      required: true,
+      min: 0
+    },
+    featured: {
+      type: Boolean,
+      default: false
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true // mongoose will add two properties to this schema - createAt and updatedAt. meaning, whenever a document is created with this schema, it will have those two porperties
+})
+
+const Campsite = mongoose.model('Campsite', campsiteSchema) // first argument should be capitalized and singular. finds/creates 'campsites/ (lowercase plural) ****confirm this****
+
+module.exports = Campsite

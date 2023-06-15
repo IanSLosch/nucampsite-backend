6ecf05f3ccba57d0d812d5c1ae168c063e5d06f5
Ian S <ianslosch@gmail.com>
2023-06-15 19:52:23 -0400

    Express Session Part 2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const mongoose = require('mongoose')
 const url = 'mongodb://localhost:27017/nucampsite'
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
-  useFindAndModify: false, 
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -49,41 +49,26 @@ app.use(session({
   resave: false, // once a sessions is created, updated and saved, it will continue to be resaved whenever a request is made for that session (even if no changes made) helps keeping the session marked as active so it doesn't get reset while user is using it
   store: new FileStore() //creates FileStore to save to the hard disk instead of just application memory
 }))
-
 //session end
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // add encrypted authentication using cookieParser
 function auth(req, res, next) {
   console.log(req.session)
 
-  if (!req.session.user) { 
-    const authHeader = req.headers.authorization
-    if (!authHeader) { // no username and pass info
-        const err = new Error('You are not authenticated!')
-        res.setHeader('WWW-Authenticate', 'Basic') // lets client know the server is requesting basic authenitcation
-        err.status = 401
-        return next(err) // passed to express error handler
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':') // Buffer is a global class in node. has a static method 'from'
-    const user = auth[0] // grabs username from 'auth' array created above
-    const pass = auth[1] // grabs password from 'auth' array created above
-    if (user === 'admin' && pass === 'password') {
-        req.session.user = 'admin' // res.cookie comes from express response object api (username, password, optional configuration values) - signed: true lets express know to use the secret key from kooie parser to create a signed cookie
-        return next() // authorized
-    } else {
-        const err = new Error('You are not authenticated!')
-        res.setHeader('WWW-Authenticate', 'Basic') // challenges the user to resubmit username and password
-        err.status = 401
-        return next(err) // passed to express error handler
-    }
+  if (!req.session.user) {
+    const err = new Error('You are not authenticated!')
+    err.status = 401
+    return next(err) // passed to express error handler
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       return next()
-    } else{
-      const err = new Error('You are not authenticated!')  
-        err.status = 401
-        return next(err)
+    } else {
+      const err = new Error('You are not authenticated!')
+      err.status = 401
+      return next(err)
     }
   }
 }
@@ -95,19 +80,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
 // defines the url endpoint for the router
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
 app.use('/campsites', campsiteRouter)
 app.use('/promotions', promotionRouter)
 app.use('/partners', partnerRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
